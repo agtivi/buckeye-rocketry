c94805c2301e2d5d3bc13d2348151284a8d16d51
agtivi <95649224+agtivi@users.noreply.github.com>
2025-08-07 00:18:03 -0400

    Animated Rocket Transition
    Transition for inital opening of site,
    text will go from "Buckeye Rocketry" to
    "From the Shoe to the stratosphere"

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import VideoLoop from "../components/videoloop";
 import RollingGallery from "../components/rollinggallery";
 import { Bebas_Neue } from "next/font/google";
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 
 const bebasNeue = Bebas_Neue({
   subsets: ['latin'],
@@ -13,9 +14,21 @@ const bebasNeue = Bebas_Neue({
 export default function Home(){
 
   const [isTitleFull, setIsTitleFull] = useState(true);
+  const [isSwitched1H, setIsSwitched1H] = useState(false);
+  const [isSwitched2H, setIsSwitched2H] = useState(false);
+  const [isFlying, setIsFlying] = useState(false);
 
   useEffect(() => {
     document.body.classList.add('overflow-hidden');
+
+    setIsFlying(true);
+
+    setTimeout(() => {
+      setIsSwitched1H(true);
+    }, 1500);
+    setTimeout(() => {
+      setIsSwitched2H(true);
+    }, 1700);
 
     setTimeout(() => {
       setIsTitleFull(false);
@@ -30,6 +43,7 @@ export default function Home(){
   return (
     <div className="mx-10 text-center">
       <div className={`relative -mx-10 bg-gray rounded-b-[30] flex flex-col items-center justify-center transition-all duration-900 ease-in-out ${isTitleFull ? "-mt-30 z-40 rounded-t-[0]" : "-mt-0 z-0 rounded-t-[100]"} `}>
+        
         <div className={`mx-10 flex flex-row text-scarlet text-center py-5 text-5xl ${bebasNeue.className}`}>
           {/* <div className="w-full flex items-center">
             <Image
@@ -41,8 +55,32 @@ export default function Home(){
             />
           </div> */}
           <h1 className={`flex transition-all duration-900 items-center ${isTitleFull ? "min-h-screen" : "min-h-[10px]"}`}>
-            {isTitleFull && "Buckeye Rocketry"}
-            {!isTitleFull && "From the Shoe to the stratosphere"}
+          
+            <motion.div
+              initial={{ x: "-100vw" }}
+              animate={isFlying ? { x: "100vw" } : {}}
+              transition={{ duration: 2, ease: "easeInOut" }}
+              style={{ width: "100vw" }}
+              className="absolute z-20"
+            >
+              <Image
+                className="rotate-90 "
+                src="/rocket-with-flame.png"
+                alt="Flying Rocket"
+                height={100}
+                width={100}
+              />
+            </motion.div>
+            <motion.div
+              initial={{ clipPath: "inset(0% 0% 0% 0%)" }}
+              animate={{ clipPath: "inset(0% 0% 0% 100%)" }}
+              transition={{ delay: 0.75, duration: 0.4, ease: "easeInOut" }}
+              style={{ width: "100vw", overflow: "hidden" }}
+              className="flex items-center content-center justify-center h-[100vh] absolute left-0 right-0 z-10 bg-gray"
+            >
+              <p className="">{"Buckeye Rocketry"}</p>
+            </motion.div>
+            <p className="absolute left-0 right-0 z-5">From the Shoe to the stratosphere</p>
           </h1>
         </div>
         <div className={`pb-10 flex items-center justify-center ${isTitleFull ? "mt-300" : "mt-0"}`}>
@@ -54,7 +92,7 @@ export default function Home(){
       </h1>
       <div className="py-5">
         <p className="px-[10vw]">
-          We started Buckeye Rocketry  to give every Ohio State student a clear way into rocketry. All majors can find a spot in our club. Whether you want to learn design, fabrication, propulsion, or even media. We have the tools and people to help you pick it up. Our goals are big, from a two‑stage launch to future high‑altitude projects, and we want anyone who’s excited to jump in and push with us. Grow your skills, add your ideas, and let’s reach higher together.
+          We started Buckeye Rocketry to give every Ohio State student a clear way into rocketry. All majors can find a spot in our club. Whether you want to learn design, fabrication, propulsion, or even media. We have the tools and people to help you pick it up. Our goals are big, from a two‑stage launch to future high‑altitude projects, and we want anyone who’s excited to jump in and push with us. Grow your skills, add your ideas, and let’s reach higher together.
         </p>
       </div>
       <h1 className={`py-5 text-5xl ${bebasNeue.className}`}>
