2468ece475c535fb20c2a81f00af1c4d9e73dafc
agtivi <95649224+agtivi@users.noreply.github.com>
2025-08-22 14:44:16 -0400

    converted videos from mp4 to webm
    attempt at trying to use less memory in website

--- src/app/components/videoloop.tsx
+++ src/app/components/videoloop.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 
-const rocketInSpace = "videos/rocketinspace.mp4";
-const rocketLaunch = "videos/rocketlaunch.mp4";
-const spaceLady = "videos/spacelady.mp4";
-
+const vids = ["videos/rocketinspace.webm", "videos/rocketlaunch.webm", "videos/spacelady.webm"];
 interface Props {
     showIntro: boolean;
 }
@@ -11,74 +8,31 @@ interface Props {
 export default function VideoLoop({showIntro}: Props){
   
     const [vidIndex, setVidIndex] = useState(0);
-    const [visible, setVisible] = useState(false);
 
     const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
 
-    const handleVideoEnd = (nextIndex: number) => {
-    if (videoRefs.current[vidIndex]) {
-        // Reset to the beginning
-        videoRefs.current[vidIndex]!.currentTime = 0;
-    }
-        setVidIndex(nextIndex);
-    };
-
     useEffect(() => {
-        if(showIntro){
-            setTimeout(() => {
-                setVisible(true);
-            }, 5500)
-        } else {
-            setVisible(true);
-        }
-    }, [showIntro])
-
-    useEffect(() => {
-        const currentVideo = videoRefs.current[vidIndex];
-        if(currentVideo) {
-            currentVideo.currentTime = 0;
-            currentVideo.play().catch((e) => {
-                console.warn("Autoplay prevented: ", e);
-            });
-        }
-    }, [vidIndex]);
+        const interval = setInterval(() => {
+            setVidIndex((prev) => (prev + 1) % vids.length);
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [])
 
 
     return(
-        <div className={`relative z-0 transition-all duration-1500 ${visible ? "opacity-100" : "opacity-0"}`}>
+        <div className={`relative z-0 transition-all duration-1500`}>
             
             <video
+                key={vidIndex}
                 className="h-[58vh] md:h-[75vh]"
-                ref={(el) => { videoRefs.current[0] = el; }}
-                style={{ objectFit:'cover', objectPosition:'top', width:'100vw', top:0, position: 'relative', display: vidIndex === 0 ? "block" : "none"}}
-                src={spaceLady}
+                ref={(el) => { videoRefs.current[vidIndex] = el; }}
+                style={{ objectFit:'cover', objectPosition:'top', width:'100vw', top:0, position: 'relative' }}
+                src={vids[vidIndex]}
                 muted
                 playsInline
                 autoPlay
-                preload="auto"
-                onEnded={() => handleVideoEnd(1)}
-            />
-            <video
-                className="h-[58vh] md:h-[75vh]"
-                ref={(el) => { videoRefs.current[1] = el; }}
-                style={{ objectFit:'cover', objectPosition:'top', width:'100vw', top:0, position: 'relative', display: vidIndex === 1 ? "block" : "none"}}
-                src={rocketLaunch}
-                muted
-                playsInline
-                autoPlay
-                preload="metadata"
-                onEnded={() => handleVideoEnd(2)}
-            />
-            <video
-                className="h-[58vh] md:h-[75vh]"
-                ref={(el) => { videoRefs.current[2] = el; }}
-                style={{ objectFit:'cover', objectPosition:'top', width:'100vw', top:0, position: 'relative', display: vidIndex === 2 ? "block" : "none"}}
-                src={rocketInSpace}
-                muted
-                playsInline
-                autoPlay
-                preload="metadata"
-                onEnded={() => handleVideoEnd(0)}
+                loop={false}
+                preload="none"
             />
             <div className="homescreen-gradient"/>
         </div>
